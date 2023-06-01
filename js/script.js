68f06a81045a47ba183df7844710868a78acf424
jaupi98 <alessio.jaupllari@maxwell.mi.it>
2023-06-01 17:40:25 +0200

    finito

--- js/script.js
+++ js/script.js
@@ -13,10 +13,20 @@ function uniqueRandomNumber(array, number){
     }
     return true;
 }
+// FNZIONE CHE PERMETTE L'INSERIMENTO DEI NUMERI DELL'UTENTE
+function getUserNumber(){
+    let userNumber = [];
+    while(userNumber.length < 5){
+        let number = parseInt(prompt('Inserisci un numero'));
+        if(!userNumber.includes(number)){
+            userNumber.push(number);
+        }
+    }
+    return userNumber;
+}
 // DEFINAMO UN ARRAY VUOTO CHE CONTIENE I NUMERI GENERATI ED UNO HE CONTERRA I NUMERI INSERITI DAL UTENTE
 
 let randomNumber= [];
-let userNumber = [];
 
 // ESEGUIRE UN CICLO FOR PER POPOLARE L'ARRAY DEI NUMERI CASUALI
 
@@ -31,3 +41,23 @@ for(let i=0; i<5; i++){
 }
 console.log(randomNumber);
 document.getElementById('random-number').innerHTML =randomNumber;
+//TIMEOUT CHE NASCONDE I NUMERI
+setTimeout(function(){
+    document.getElementById('random-number').innerHTML = '';
+    document.getElementById('message').innerHTML = ' Inserire i numeri che ti ricordi';
+},5000)
+
+setTimeout(function(){
+    // RICHIAMO LA FUNZIONE CHE PERMETTE L'INSERIMENTO DEI NUMERI DA PARTE DEL UTENTE
+    let userNumber = getUserNumber();
+    let guessedNumber = [];
+    let score = 0;
+    console.log(userNumber);
+    for(let i=0; i<randomNumber.length; i++){
+        if(randomNumber.includes(userNumber[i])){
+            guessedNumber.push(userNumber[i]);
+            score++;
+        }
+    }
+    document.getElementById('message').innerHTML =  `hai indovinato ${score} numeri . I numeri coretti soo ${guessedNumber}` ;
+},6000);
